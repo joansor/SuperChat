7a0d0d6d56857ab7792c9b4f847cc70ad3470345
joansor <sor.joan@gmail.com>
2020-05-08 16:07:35 +0200

    recuere la val du form

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-var http = require('http');
-var fs = require('fs');
-let express = require('express');
+var http = require("http");
+var fs = require("fs");
+let express = require("express");
 
 let app = express();
 
 // Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+var server = http.createServer(function (req, res) {
+  fs.readFile("./index.html", "utf-8", function (error, content) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(content);
+  });
 });
 
 // Chargement de socket.io
-var io = require('socket.io').listen(server);
+var io = require("socket.io").listen(server);
 
+io.sockets.on("connection", function (socket, pseudo) {
+  // Quand un client se connecte, on lui envoie un message
+  socket.emit("message", "Vous êtes bien connecté !");
+  // On signale aux autres clients qu'il y a un nouveau venu
+  //socket.broadcast.emit('pseudo', pseudo + 'vient de se connecter ! ');
 
-io.sockets.on('connection', function (socket, pseudo) {
-    // Quand un client se connecte, on lui envoie un message
-    socket.emit('message', 'Vous êtes bien connecté !');
-    // On signale aux autres clients qu'il y a un nouveau venu
-    //socket.broadcast.emit('pseudo', pseudo + 'vient de se connecter ! ');
+  // Dès qu'on nous donne un pseudo, on le stocke en variable de session
+  socket.on("pseudo", function (pseudo) {
+    socket.pseudo = pseudo; //recupere le speudo
+    socket.emit("pseudo", pseudo); // on le renvoie au meme utilisateur
+    socket.broadcast.emit("pseudo", pseudo); //on renvoie le pseudo a tout les autres
+  });
 
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session
-    socket.on('pseudo', function(pseudo) {
-        socket.pseudo = pseudo; //recupere le speudo
-        socket.emit("pseudo", pseudo);// on le renvoie au meme utilisateur
-        socket.broadcast.emit("pseudo",pseudo);//on renvoie le pseudo a tout les autres
-    });
+  //ajout un message par le formulaire
+  app.post("/traitement", function (req, res) {
 
     // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
-    socket.on('message', function (message) {
-        // On récupère le pseudo de celui qui a cliqué dans les variables de session
-        
-        socket.emit(socket.pseudo +':'+' '+ message);
-        socket.broadcast.emit("message",message)
+    socket.on("message", function (message) {
+        console.log(message);
+      // On récupère le pseudo de celui qui a cliqué dans les variables de session
+      socket.emit(socket.pseudo + ":" + " " + message);
+      socket.broadcast.emit("message", message);
 
-        redirect('/');
-    }); 
+      res.redirect('/');
+    });
+  });
 });
-
 
 server.listen(8080);
